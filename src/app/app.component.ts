638de2eda625619cfaee8c92f2cd3a7caadf363c
jethro <jethro.david@tablogs.com.au>
2025-04-06 12:57:53 +0800

    Refactor application structure and integrate Firebase authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Storage } from '@ionic/storage-angular';
+import { FirebaseAuthenticationService } from './core';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +9,20 @@ import { Storage } from '@ionic/storage-angular';
   standalone: false,
 })
 export class AppComponent {
-  constructor(private storage: Storage) {}
+  constructor(
+    private readonly firebaseAuthenticationService: FirebaseAuthenticationService,
+    private storage: Storage
+  ) {
+    this.initializeApp();
+  }
 
   async ngOnInit() {
     // If using a custom driver:
     // await this.storage.defineDriver(MyCustomDriver)
     await this.storage.create();
+  }
+
+  private async initializeApp(): Promise<void> {
+    await this.firebaseAuthenticationService.initialize();
   }
 }
